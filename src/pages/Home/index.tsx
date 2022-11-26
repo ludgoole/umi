6151030b30317fb0b96da6f380edbf4167168bce
hengxiao <hengxiao@iflytek.com>
2022-11-27 01:35:39 +0800

    feat(home): 去除objectId

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,9 @@ import styles from './index.less';
 import { request } from '@umijs/max';
 
 request<API.Result_PageInfo_UserInfo__>(
-  'https://njr7h2zt.lc-cn-n1-shared.com/1.1/classes/stock/63822c4c1d35662164c70649',
+  // /1.1/classes/<className>/<objectId>
+  // 63822c4c1d35662164c70649
+  'https://njr7h2zt.lc-cn-n1-shared.com/1.1/classes/stock',
   {
     // POSt 增
     // DELETE 删
